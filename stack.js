e5ed32e5b46e078e62456e5a197f76594b46368a
Blake C <bcortri1@kent.edu>
2023-09-05 14:55:56 -0400

    Stack and Queue Classes complete

---
+++ stack.js
@@ -0,0 +1,65 @@
+/** Node: node for a stack. */
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+/** Stack: chained-together nodes where you can
+ *  remove from the top or add to the top. */
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    /** push(val): add new value to end of the stack. Returns undefined. */
+
+    push(val) {
+        let node = new Node(val)
+        if (this.isEmpty()) {
+            this.first = node;
+            this.last = node;
+            
+        }
+        else {
+            node.next = this.first;
+            this.first = node;
+
+        }
+        this.size++;
+    }
+
+    /** pop(): remove the node from the top of the stack
+     * and return its value. Should throw an error if the stack is empty. */
+
+    pop() {
+        if (this.isEmpty()) {
+            throw new Error("Stack is empty")
+        }
+        else{
+            let removed = this.first;
+            this.first = this.first.next;
+            this.size--;
+            return removed.val;
+        }
+    }
+
+    /** peek(): return the value of the first node in the stack. */
+
+    peek() {
+        return this.first.val;
+    }
+
+    /** isEmpty(): return true if the stack is empty, otherwise false */
+
+    isEmpty() {
+        return !this.size;
+    }
+}
+
+module.exports = Stack;
